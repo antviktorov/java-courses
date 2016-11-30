eafc4ba70d50bbe59ec578e20112464f9f63b7e3
antonviktorov <antvik85@gmail.com>
2016-11-30 16:50:17 +0300

    change: chapter 1.3 Add JavaDoc

--- chapter1/jvm/src/com/antviktorov/Calculate.java
+++ chapter1/jvm/src/com/antviktorov/Calculate.java
@@ -1,6 +1,15 @@
 package com.antviktorov;
 
+/**
+* First class in Java lol
+* @author Anton Viktorov
+* @since 30.11.2016
 public class Calculate {
+
+	/**
+	* Java CLI start point
+	* @param arg - array of arguments provided in CLI
+        */
 	public static void main(String[] arg) {
 		System.out.println("Hello World!");
 	}

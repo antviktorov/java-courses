5678fb101f3ecd79d26c157e1bb7769ca392b357
antonviktorov <antvik85@gmail.com>
2016-12-04 17:28:20 +0300

    Fix: Chapter 2.3 Exclude tests from check styles

--- chapter_001/src/test/java/com/antviktorov/CalculatorTest.java
+++ chapter_001/src/test/java/com/antviktorov/CalculatorTest.java
@@ -13,63 +13,13 @@ import static org.junit.Assert.assertThat;
 public class CalculatorTest {
 
     /**
-     * Addition result.
-     */
-    private static final double ADDITION_RESULT          = 4.0;
-    /**
-     * First argument.
-     */
-    private static final double ADDITION_FIRST_ARGUMENT  = 2;
-    /**
-     * Second argument.
-     */
-    private static final double ADDITION_SECOND_ARGUMENT = 2;
-    /**
-     * Substraction result.
-     */
-    private static final double SUBSTRACTION_RESULT          = 5.0;
-    /**
-     * First argument.
-     */
-    private static final double SUBSTRACTION_FIRST_ARGUMENT  = 10;
-    /**
-     * Second argument.
-     */
-    private static final double SUBSTRACTION_SECOND_ARGUMENT = 5;
-
-    /**
-     * Multiplication result.
-     */
-    private static final double MULTIPLUCATION_RESULT                       = 10.0;
-    /**
-     * First argument.
-     */
-    private static final double MULTIPLUCATION_SUBSTRACTION_FIRST_ARGUMENT  = 2;
-    /**
-     * Second argument.
-     */
-    private static final double MULTIPLUCATION_SUBSTRACTION_SECOND_ARGUMENT = 5;
-
-    /**
-     * Division result.
-     */
-    private static final double DIVISION_RESULT = 3.0;
-    /**
-     * First argument.
-     */
-    private static final double DIVISION_SUBSTRACTION_FIRST_ARGUMENT  = 9;
-    /**
-     * Second argument.
-     */
-    private static final double DIVISION_SUBSTRACTION_SECOND_ARGUMENT = 3;
-    /**
      * Addition Test 2 + 2 = 4.
      */
     @Test
     public void whenAdditionTwoPlusTwoThenFour() {
         Calculator calculator = new Calculator();
-	calculator.calculate("+", ADDITION_FIRST_ARGUMENT, ADDITION_SECOND_ARGUMENT);
-	assertThat(calculator.getResult(), is(ADDITION_RESULT));
+	    calculator.calculate("+", 2, 2);
+	    assertThat(calculator.getResult(), is(4.0));
     }
 
 
@@ -79,8 +29,8 @@ public class CalculatorTest {
     @Test
     public void whenSubstactionTenMinusFiveThenFive() {
         Calculator calculator = new Calculator();
-        calculator.calculate("-", SUBSTRACTION_FIRST_ARGUMENT, SUBSTRACTION_SECOND_ARGUMENT);
-        assertThat(calculator.getResult(), is(SUBSTRACTION_RESULT));
+        calculator.calculate("-", 10, 5);
+        assertThat(calculator.getResult(), is(5.0));
     }
 
     /**
@@ -88,13 +38,9 @@ public class CalculatorTest {
      */
     @Test
     public void whenMultiplicationTwoMultiplyFiveThenTen() {
-   	Calculator calculator = new Calculator();
-   	calculator.calculate(
-             "*",
-             MULTIPLUCATION_SUBSTRACTION_FIRST_ARGUMENT,
-      	     MULTIPLUCATION_SUBSTRACTION_SECOND_ARGUMENT
-   	);
-   	assertThat(calculator.getResult(), is(MULTIPLUCATION_RESULT));
+        Calculator calculator = new Calculator();
+        calculator.calculate("*", 2, 5);
+        assertThat(calculator.getResult(), is(10.0));
    }
 
    /**
@@ -103,11 +49,7 @@ public class CalculatorTest {
    @Test
    public void whenNineDivisitionThreeThenThree() {
        Calculator calculator = new Calculator();
-       calculator.calculate(
-            "/",
-            DIVISION_SUBSTRACTION_FIRST_ARGUMENT,
-            DIVISION_SUBSTRACTION_SECOND_ARGUMENT
-       );
-       assertThat(calculator.getResult(), is(DIVISION_RESULT));
+       calculator.calculate("/", 9, 3);
+       assertThat(calculator.getResult(), is(3.0));
     }
 }

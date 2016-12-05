4c8c6371d35b6c6aa94068f5bcd4dd8843c39fb1
antonviktorov <antvik85@gmail.com>
2016-12-05 19:31:28 +0300

    Add: chapter 4.3 add Paind with pyramid drawing

---
+++ chapter_001/src/main/java/com/antviktorov/Paint.java
@@ -0,0 +1,38 @@
+package com.antviktorov;
+
+/**
+ * Paint class for pyramid building.
+ * @author Anton Viktorov
+ * @since 1.0
+ */
+public class Paint {
+    /**
+     * Build pyramid.
+     * @param h - pyramid height
+     * @return
+     */
+    public  String piramid(int h) {
+        //Return emty string if height = 0
+        if (h == 0) return "";
+        //Return one height pyramid
+        if (h == 1) return "^";
+
+        int level = h;
+        StringBuilder builder = new StringBuilder();
+
+        while (level > 0) {
+            //Fill with spaces before ^
+            for (int i = 1; i <= level - 1; i++) {
+                builder.append(" ");
+            }
+            //Fill with ^ after spaces
+            for (int i = 1; i <= h - level + 1; i++) {
+                builder.append("^");
+            }
+            level--;
+            builder.append("\n");
+        }
+
+        return builder.toString();
+    }
+}
---
+++ chapter_001/src/test/java/com/antviktorov/PaintTest.java
@@ -0,0 +1,49 @@
+package com.antviktorov;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Paint Test class.
+ *
+ * @author Anton Viktorov
+ * @since 1.0
+ */
+public class PaintTest {
+
+    /**
+     * Build pyramid with height = 0
+     */
+    @Test
+    public void whenBuidPyramidHeightZeroThenPyramid() {
+        Paint paint = new Paint();
+        assertEquals(paint.piramid(0), "");
+    }
+
+    /**
+     * Build pyramid with height = 1
+     */
+    @Test
+    public void whenBuidPyramidHeightOneThenPyramid() {
+        Paint paint = new Paint();
+        assertEquals(paint.piramid(1), "^");
+    }
+
+    /**
+     * Build pyramid with height = 2
+     */
+    @Test
+    public void whenBuidPyramidHeightTwoThenPyramid() {
+        Paint paint = new Paint();
+        assertEquals(paint.piramid(2), " ^\n^^\n");
+    }
+
+    /**
+     * Build pyramid with height = 3
+     */
+    @Test
+    public void whenBuidPyramidHeightThreeThenPyramid() {
+        Paint paint = new Paint();
+        assertEquals(paint.piramid(3), "  ^\n ^^\n^^^\n");
+    }
+}

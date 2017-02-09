9e14dbf626bed5d2ff5c0776e48caa7fdc23feac
antonviktorov <antvik85@gmail.com>
2017-02-09 22:36:16 +0300

    added: check isNumber method in StreamChecher

--- chapter_003/io/src/main/java/com/antviktorov/io/StreamChecker.java
+++ chapter_003/io/src/main/java/com/antviktorov/io/StreamChecker.java
@@ -1,9 +1,35 @@
 package com.antviktorov.io;
 
+import java.io.InputStream;
+import java.io.IOException;
+
 /**
  * StreamChecker.
  * @author antonviktorov
  * @since 1.0
  */
 public class StreamChecker {
+    /**
+     * StreamChecker constructor.
+     */
+    public StreamChecker() {
+    }
+
+    /**
+     * Check stream is even.
+     * @param in - stream
+     * @return are all stream bytes even
+     * @throws IOException
+     */
+    Boolean isNumber(InputStream in) throws IOException {
+        int oneByte;
+        Boolean result = true;
+        while ((oneByte = in.read()) != -1) {
+            if (oneByte % 2 != 0) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
 }
--- chapter_003/io/src/test/java/com/antviktorov/io/StreamCheckerTest.java
+++ chapter_003/io/src/test/java/com/antviktorov/io/StreamCheckerTest.java
@@ -1,9 +1,36 @@
 package com.antviktorov.io;
 
+import org.junit.Test;
+import java.io.*;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * StreamCheckerTest.
  * @author antonviktorov
  * @since 1.0
  */
 public class StreamCheckerTest {
+    @Test
+    public void whenTestNotEvenByteStreamThenTrue() {
+        byte[] bytes = new byte[] {1,2,3,4,5};
+        try (BufferedInputStream in = new BufferedInputStream(new ByteArrayInputStream(bytes))){
+            StreamChecker checker = new StreamChecker();
+            assertThat(checker.isNumber(in), is(false));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void whenTestEvenByteStreamThenTrue() {
+        byte[] bytes = new byte[] {2,4,6,8,10};
+        try (BufferedInputStream in = new BufferedInputStream(new ByteArrayInputStream(bytes))){
+            StreamChecker checker = new StreamChecker();
+            assertThat(checker.isNumber(in), is(true));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
